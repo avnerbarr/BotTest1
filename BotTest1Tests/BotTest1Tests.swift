c9279ef19e76859409e1f4be4ae196fa9ed9acd0
Avner Barr <avner@insert.io>
2015-10-22 16:23:30 +0300

    Initial Commit

---
+++ BotTest1Tests/BotTest1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BotTest1Tests.swift
+//  BotTest1Tests
+//
+//  Created by Avner Barr on 10/22/15.
+//  Copyright © 2015 Avner Barr. All rights reserved.
+//
+
+import XCTest
+@testable import BotTest1
+
+class BotTest1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
